6977615393af566034862922e77fd003c1971071
xanderMMV <241322@virtualwindow.co.za>
2024-05-15 17:54:19 +0200

    jerry

---
+++ js/script.js
@@ -0,0 +1,20 @@
+
+function doNotWave(){
+    var img = document.getElementById('jerry');
+    img.src = './assets/Mad.png';
+}
+
+function waveBack(){
+    var img = document.getElementById('jerry');
+    img.src = './assets/Happy.png';
+}
+
+function saySorry(){
+    var img = document.getElementById('jerry');
+    img.src = './assets/Forgive.png';
+}
+
+function reset(){
+    var img = document.getElementById('jerry');
+    img.src = './assets/Hallo.png';
+}
